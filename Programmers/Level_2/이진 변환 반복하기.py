964826355bcbd647c2d05851f6aa290615ba4e96
지수 박 <alyssapark96@gmail.com>
2021-06-04 20:23:58 +0900

    2021/06/04 practice

---
+++ Programmers/Level_2/이진 변환 반복하기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = [0,0]
+    
+    while s != '1':
+        zero = s.count('0')
+        s = s.replace('0','')
+        s = format(len(s), 'b')
+        answer[0] += 1
+        answer[1] += zero
+    return answer
